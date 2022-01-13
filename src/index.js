267cd5462f8d1e05c1c182127395ca0d21dab819
Julia-itcourse <juliaartemenko93@gmail.com>
2022-01-13 16:12:12 +0100

    refactor: refactor code

--- src/index.js
+++ src/index.js
@@ -46,29 +46,21 @@ module.exports = function toReadable(number) {
   const isTwoDigit = number / 10 >= 1 && number / 100 < 1;
   const isThreeDigit = number / 100 >= 1;
 
-  // for numbers 0 - 9
-
   if (isOneDigit) {
     return (result = numberMap.units[number]);
   }
 
-  //for 2 digit numbers
-
   const numArr = Array.from(String(number), Number);
 
   if (isTwoDigit) {
-    //for numbers 10, 20, 30, 40 etc
-
     if (numArr[1] === 0) {
       return (result = numberMap.dozens[numArr[0]]);
     }
 
-    //for numbers 11 - 19
     if (numArr[0] === 1) {
       return (result = numberMap.teens[numArr[1]]);
     }
 
-    //for numbers 25, 34, 41 etc
     resultArray[0] = numberMap.dozens[numArr[0]];
     resultArray[1] = numberMap.units[numArr[1]];
     return (result = resultArray.join(" "));
@@ -95,7 +87,6 @@ module.exports = function toReadable(number) {
       return (result = resultArray.join(" "));
     }
 
-    //714
     if (numArr[1] === 1 && numArr[2] !== 0) {
       resultArray[0] = numberMap.units[numArr[0]];
       resultArray[1] = "hundred";
